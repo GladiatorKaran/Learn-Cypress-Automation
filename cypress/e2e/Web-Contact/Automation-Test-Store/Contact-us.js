d35add4b34d8dcd54f41cef10e464a5d2fbb1230
GladiatorKaran <karankiruba215@gmail.com>
2023-09-19 23:36:58 +0530

    2nd Push

---
+++ cypress/e2e/Web-Contact/Automation-Test-Store/Contact-us.js
@@ -0,0 +1,14 @@
+///  <reference types="Cypress" />
+
+describe("Test Contstc us from via wedriverUni", () => {
+    it("Submit the Contact us form",() => {
+        //cypress code
+        cy.visit("http://www.automationteststore.com");
+        
+    });
+    it("Submit the Contact us form by mandatory fields without",() => {
+        //cypress code
+        
+    });
+});
+
